1df8eb012076ab38acd5c4da76f44431c6bb37b9
jwj9127 <jwj9127@naver.com>
2024-02-08 18:25:08 +0900

    회원정보수정,공모전상세보기진행중

--- src/pages/SignUI.js
+++ src/pages/SignUI.js
@@ -21,20 +21,6 @@ export default function MyPage() {
     const [address, setAddress] = useState('');
     const [gender, setGender] = useState('');
     const [tendency, setTendency] = useState('');
- 
-    // 유저가 선택한 gender와 tendency를 state로 관리
-    const [selectedGender, setSelectedGender] = useState('');
-    const [selectedTendency, setSelectedTendency] = useState('');
-
-    // gender 선택 시 호출되는 함수
-    const handleGenderChange = (e) => {
-        setSelectedGender(e.target.value);
-    };
-    
-    // tendency 선택 시 호출되는 함수
-    const handleTendencyChange = (e) => {
-        setSelectedTendency(e.target.value);
-    };
 
     // 유저가 선택한 gender와 tendency를 state로 관리
     const [selectedGender, setSelectedGender] = useState('');
@@ -54,7 +40,7 @@ export default function MyPage() {
     const [pwMessage, setPwMessage] = useState('');
     const [nameMessage, setNameMessage] = useState('');
     const [phoneMessage, setPhoneMessage] = useState('');
-    
+
     // 유효성 검사
     const [isId, setIsId] = useState(false);
     const [isPw, setIsPw] = useState(false);
@@ -138,16 +124,10 @@ export default function MyPage() {
         try {
             axios({
                 method: 'post',
-                url: `/user/join`,
-<<<<<<< HEAD
+                url: '/user/join',
                 data: { UserDto: signUpForm },
                 headers: {
                     'Content-Type': 'application/json'
-=======
-                data: { User: signUpForm }, 
-                headers: {
-                    'Content-Type': 'application/json' 
->>>>>>> ee44b798bfb38aabcb656ee4aa43d50457907d3f
                 }
             }).then(result => {
                 if (result.status == 200) {
@@ -179,17 +159,8 @@ export default function MyPage() {
         try {
             axios({
                 method: 'post',
-                url: `/user/idCheck`,
-<<<<<<< HEAD
-                data: { id: id },
-                headers: {
-                    'Content-Type': 'application/json'
-=======
-                data: { id: id }, 
-                headers: {
-                    'Content-Type': 'application/json' 
->>>>>>> ee44b798bfb38aabcb656ee4aa43d50457907d3f
-                }
+                url: '/user/idCheck',
+                data: id
             }).then(result => {
                 if (result.status == 200) {
                     Swal.fire({
@@ -620,13 +591,8 @@ export default function MyPage() {
                             <div class="login__box">
                                 <div>
                                     <FontAwesomeIcon icon={faVenusMars} />
-<<<<<<< HEAD
                                     <input type="radio" id="genderMale" name="gender" value="남자" class="gender_input" checked={selectedGender === "남자"} onChange={handleGenderChange} />남자
                                     <input type="radio" id="genderFemale" name="gender" value="여자" class="gender_input" checked={selectedGender === "여자"} onChange={handleGenderChange} />여자
-=======
-                                    <input type="radio" id="genderMale" name="gender" value="남자" class="gender_input" checked={selectedGender === "남자"} onChange={handleGenderChange}/>남자
-                                    <input type="radio" id="genderFemale" name="gender" value="여자" class="gender_input" checked={selectedGender === "여자"} onChange={handleGenderChange}/>여자
->>>>>>> ee44b798bfb38aabcb656ee4aa43d50457907d3f
                                 </div>
                             </div>
 
